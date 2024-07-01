7c51edc728a39e3daff5c4c5be424caeaa8146cd
Al Berez <al.berez@broadcom.com>
2024-07-01 12:13:55 -0700

    WIP: test in a-b fork

--- ci/scripts/hijack-act.sh
+++ ci/scripts/hijack-act.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-image_id=$(docker ps --format=json | jq --slurp '.[] | select(.Image == "cfcli/cli-release-base") | .ID' --raw-output)
+image_id=$(docker ps --format=json | jq --slurp '.[] | select(.Image == "ghcr.io/a-b/cf-cli-release-ci:main-latest") | .ID' --raw-output)
 docker exec --interactive --tty "${image_id}" /bin/bash
